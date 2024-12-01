231980ae7c2b222c158be9a9c19d23db18723106
Monchavin Pinthong <monchavin.pi@gmail.com>
2024-12-01 12:52:11 +0700

    Expected display error message when empty password.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:widget_testing/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets(
+      'Should display error message "Please enter password." when user clicks submit button with empty password',
+      (WidgetTester tester) async {
+    // Arrange
+    await tester.pumpWidget(const MaterialApp(home: PasswordValidationPage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Act
+    final passwordField = find.byType(TextField).first;
+    final submitButton = find.text('Submit');
+    await tester.enterText(passwordField, '');
+    await tester.tap(submitButton);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Assert
+    expect(find.text('Please enter password.'), findsOneWidget);
   });
 }
